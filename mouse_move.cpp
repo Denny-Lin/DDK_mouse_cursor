5fac846861d2173cb6e2037ee4e6c9ac405a27bb
DDK <67073582+Denny-Lin@users.noreply.github.com>
2021-07-18 15:05:03 +0800

    Add files via upload

--- mouse_move.cpp
+++ mouse_move.cpp
@@ -16,56 +16,52 @@ int main(){
 			
 	while(1){		
 		cout<<"Please input 's'(start recording) or 'e'(execute recording):"<<endl;
-		hit = kbhit();
-		hit=0;
 		
-		if(!hit){ 
-			hit=getch(); 
-			
-			cout<<"Input: "<<(char)hit<<"\t"<<hit<<endl;
-			
-			if(hit == 's' || hit == 'S'){
-				cout<<"Recording."<<endl; 			
-				cout<<"Press 'c' to stop."<<endl; 
-						
-				while(1){					
-					GetCursorPos(&xypos);
-					cout<<i<<":\t"<<"X:"<<xypos.x<<"\tY:"<<xypos.y<<endl;
-					
-					array_x[i]=xypos.x;
-					array_y[i]=xypos.y;
-					i++;
-					
-					Sleep(100);
-					
-					if(kbhit()) hit=getch();
-					if(hit == 'c' || hit == 'C') break;
-					if(i>=Max-1) break;	
-				}
-				
-				i=0;
-				cout<<"Stop recording."<<endl;	
-			}
-			else if(hit == 'e' || hit == 'E'){
-				cout<<"Moving."<<endl;
+		hit = getch();
 							
-				while(!kbhit()){						
-					SetCursorPos(array_x[i],array_y[i]);
-					cout<<i<<":\t"<<"X:"<<array_x[i]<<"\tY:"<<array_y[i]<<endl;
+		cout<<"Input: "<<(char)hit<<"\t"<<hit<<endl;
+		
+		if(hit == 's' || hit == 'S'){
+			cout<<"Recording."<<endl; 			
+			cout<<"Press 'c' to stop."<<endl; 
 					
-					i++;
-					
-					Sleep(100);
-					
-					if(array_x[i] == 0 && array_y[i] == 0) break;	
-					
-					if(i>=Max-1) break;			
-				}
+			while(1){					
+				GetCursorPos(&xypos);
+				cout<<i<<":\t"<<"X:"<<xypos.x<<"\tY:"<<xypos.y<<endl;
 				
-				i=0;
-				cout<<"Stop moving."<<endl;	
-			} 
-		}	
+				array_x[i]=xypos.x;
+				array_y[i]=xypos.y;
+				i++;
+				
+				Sleep(100);
+				
+				if(kbhit()) hit=getch();
+				if(hit == 'c' || hit == 'C') break;
+				if(i>=Max-1) break;	
+			}
+			
+			i=0;
+			cout<<"Stop recording."<<endl;	
+		}
+		else if(hit == 'e' || hit == 'E'){
+			cout<<"Moving."<<endl;
+						
+			while(!kbhit()){						
+				SetCursorPos(array_x[i],array_y[i]);
+				cout<<i<<":\t"<<"X:"<<array_x[i]<<"\tY:"<<array_y[i]<<endl;
+				
+				i++;
+				
+				Sleep(100);
+				
+				if(array_x[i] == 0 && array_y[i] == 0) break;	
+				
+				if(i>=Max-1) break;			
+			}
+			
+			i=0;
+			cout<<"Stop moving."<<endl;	
+		} 
 	}
 	
 	return 0;
