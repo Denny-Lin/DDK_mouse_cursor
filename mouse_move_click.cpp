3e0051231d6bc06984e37fd5ea341032ab99200c
DDK <67073582+Denny-Lin@users.noreply.github.com>
2021-07-23 14:31:10 +0800

    Add files via upload

--- mouse_move_click.cpp
+++ mouse_move_click.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <windows.h>
 #define Max 60000
+
 #define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0) 
 
 using namespace std;
@@ -32,7 +33,7 @@ int main(){
 							
 		cout<<"Input: "<<(char)mouse.kb_hit<<"\t"<<mouse.kb_hit<<endl;
 		
-		if(mouse.kb_hit == 's' || mouse.kb_hit == 'S'){
+		if(mouse.kb_hit == 's' || mouse.kb_hit == 'S' || KEY_DOWN(VK_F1)){
 			cout<<"Recording."<<endl; 			
 			cout<<"Press 'c' to stop."<<endl; 
 					
@@ -44,34 +45,39 @@ int main(){
 				mouse.cursor[mouse.i].y=mouse.xypos.y;
 
 				if (KEY_DOWN(MOUSE_MOVED)){
-            		printf("Mouse clicked.");
+            		printf("Mouse clicked.\n");
             		mouse.cursor[mouse.i].click=1;
         		}
+        		else{
+        			mouse.cursor[mouse.i].click=0;
+				}
 				
 				mouse.i++;
 				
 				Sleep(100);
 				
 				if(kbhit()) mouse.kb_hit=getch();
-				if(mouse.kb_hit == 'c' || mouse.kb_hit == 'C') break;
+				if(mouse.kb_hit == 'c' || mouse.kb_hit == 'C' || KEY_DOWN(VK_F2)) break;
 				if(mouse.i>=Max-1) break;	
 			}
 			
 			mouse.i=0;
 			cout<<"Stop recording."<<endl;	
 		}
-		else if(mouse.kb_hit == 'e' || mouse.kb_hit == 'E'){
+		else if(mouse.kb_hit == 'e' || mouse.kb_hit == 'E' || KEY_DOWN(VK_F3)){
 			cout<<"Moving."<<endl;
 						
 			while(!kbhit()){						
 				SetCursorPos(mouse.cursor[mouse.i].x,mouse.cursor[mouse.i].y);
 				cout<<mouse.i<<":\t"<<"X:"<<mouse.cursor[mouse.i].x<<"\tY:"<<mouse.cursor[mouse.i].y<<endl;
-				
+
 				if (mouse.cursor[mouse.i].click==1){
-            		printf("Mouse clicked.");
-            		mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, mouse.cursor[mouse.i].x, mouse.cursor[mouse.i].y, 0, 0);       		
-        		}
-				
+            		printf("Mouse clicked.\n");
+            		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);   
+					Sleep(50);  
+					mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);  
+					Sleep(50); 	
+        		}				
 				
 				mouse.i++;
 				
